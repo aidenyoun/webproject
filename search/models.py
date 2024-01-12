e6eeb6e9eafece505d2494a34c7ea07613560cf8
Yeowon Youn <younyw15@gmail.com>
2024-01-12 20:10:59 +0900

    2024-01-11, 08:10 AM 약품명으로 검색하기 기능 구현

--- search/models.py
+++ search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Medicine(models.Model):
+    name = models.CharField(max_length=200) # 약품명
+    description = models.TextField() # 약품 설명
+    image = models.ImageField(upload_to='medicines/', blank=True, null=True) # 약품 사진
+
+    def __str__(self):
+        return self.name
--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'search'
 
 urlpatterns = [
     path('', views.search, name='search'),
-]
+    path('name_search/', views.name_search, name='name_search'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
